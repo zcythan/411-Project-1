b296fd2c88ce76a97f761da48ad29c77c320c298
zcythan <jferrick@umich.edu>
2023-09-25 22:37:09 -0400

    more basics

---
+++ SBD.py
@@ -0,0 +1,17 @@
+
+class FeatExt:
+    def __init__(self):
+        self.x = 0
+
+class AccCalc:
+    def __init__(self):
+        self.x = 0
+
+def main():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    main()
+
+
